aca42ab13f419c257048f97e3afe71d60e46d08e
Minu Kim <minumoseskim@gmail.com>
2021-12-13 17:09:46 -0500

    Set up skeleton route handlers for index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+const { Meme } = require("../db/models");
+
+const asyncHandler = (handler) => (req, res, next) => {
+  return handler(req, res, next).catch(next);
+};
+
+/* GET home page -- default sorted by likes. */
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    // fetch memes by most comments
+    // const trendingMemes = Meme.findAll();
+
+    // fetch memes by likes
+    // const bestMemes = Meme.findAll();
+
+    res.render("index", { title: "Memehub" });
+  })
+);
+
+// GET home page sorted by recent
+router.get("/recent", (req, res, next) => {
+  res.render("index", { title: "Memehub" });
+});
+
+// GET home page sorted by most comments
+router.get("/hot", (req, res, next) => {
+  res.render("index", { title: "Memehub" });
+});
+
+// GET following feed
+router.get("/you", (req, res, next) => {
+  res.render("index", { title: "Memehub" });
 });
 
 module.exports = router;
