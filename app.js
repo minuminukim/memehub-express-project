8348946c923cadf9f5ee977c3515f1d8f50dc06d
Minu Kim <minumoseskim@gmail.com>
2021-12-13 21:11:54 -0500

    DRY up users.js by moving csrfProtection and asyncHandler into utils module

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const store = new SequelizeStore({ db: sequelize });
 
 app.use(
   session({
-    secret: "superSecret",
+    secret: sessionSecret,
     store,
     saveUninitialized: false,
     resave: false,
