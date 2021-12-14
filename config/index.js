851dffdde26e71b7159072f3e1e1429585e56a2d
w-duffy <82291955+w-duffy@users.noreply.github.com>
2021-12-13 19:22:04 -0600

    Revert "Home page"

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 module.exports = {
-  environment: process.env.NODE_ENV || "development",
+  environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8080,
-  sessionSecret: process.env.SESSION_SECRET,
   db: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
