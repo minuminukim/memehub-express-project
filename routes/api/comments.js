42ec36a3327591f6c74ecbb6ee6e31e21e6e90ba
davigravi <davidlee777@gmail.com>
2021-12-15 11:47:17 -0600

    moved comments.js to api folder

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -2,5 +2,6 @@ const express = require("express");
 
 const db = require("../db/models");
 
+const { csrfProtection, asyncHandler } = require("../utils");
 const { requireAuth } = require("../auth");
 const router = express.Router();
