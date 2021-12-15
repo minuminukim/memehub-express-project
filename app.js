61f9644845e80dd4a94f6ff0a28e342d39cfdf26
Minu Kim <minumoseskim@gmail.com>
2021-12-15 12:08:42 -0500

    Hello, /api/ route

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const signInRouter = require("./routes/sign-in");
 const memesRouter = require("./routes/memes");
+const apiRouter = require("./routes/api");
 
 const app = express();
 
@@ -46,6 +47,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/memes", memesRouter);
 app.use("/sign-in", signInRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
