b9bd74de8d692b17e577d86526d92d88bdf286f4
anthony <awadams198@gmail.com>
2021-12-15 20:06:15 -0500

    started route and listener

---
+++ public/javascripts/likes.js
@@ -0,0 +1,12 @@
+// window.addEventListener("load", (event) => {
+console.log("likes javascript!");
+// });
+
+const addLike = document.querySelector(".likeButton");
+
+
+addLike.addEventListener("click", async (e) => {
+  e.target.id
+
+  const like = await like.create({ userId: req.user.id, memeId: req.meme.id });
+});
---
+++ routes/api/likes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { validationResult } = require("express-validator");
+
+const db = require("../../db/models");
+const { csrfProtection, asyncHandler } = require("../../utils");
+
+const { requireAuth } = require("../../auth");
+const { like } = require("sequelize/types/lib/operators");
+
+const router = express.Router();
+
+
+router.post("/api/likes"),
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const likeMeme = await db.Like.findOne({
+      where: {
+      userId: {
+      [Op.]: <value>
+      }
+      },
+     });
+if (db.)
+    const like = db.Like.build({
+      userId: res.locals.user.id,
+      memeId: memeId,
+      // like++
+    });
+
+    // if a like does not exist
+    // build a like
+    // if a like exists
+    // destroy a like
+
+
+
+module.exports = router;
