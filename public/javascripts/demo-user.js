19d23ac0e36c1cab8d92b4bf20e92eeed3420082
Minu <minumoseskim@gmail.com>
2021-12-16 05:13:38 -0500

    Add demo user event to sign in form

---
+++ public/javascripts/demo-user.js
@@ -0,0 +1,13 @@
+const demoSignIn = () => {
+  const button = document.querySelector(".demo-button");
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const emailField = document.querySelector("#email");
+    const passwordField = document.querySelector("#password");
+    emailField.value = "hello@world.com";
+    passwordField.value = "password";
+  });
+};
+
+window.onload = () => demoSignIn();
