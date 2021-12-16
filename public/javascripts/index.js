aab3afdbd77857ecedd3a850ed04663cc5bd60ff
Minu Kim <minumoseskim@gmail.com>
2021-12-16 01:31:57 -0500

    Add click event listener on dropdown: currently only working on homepage

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,17 @@
+const toggleDropdown = () => {
+  const button = document.querySelector(".dropdown-button");
+  button.addEventListener("click", (event) => {
+    const dropdown = document.querySelector(".dropdown-container");
+
+    if (dropdown.style.display === "none") {
+      dropdown.style.display = "flex";
+    } else {
+      dropdown.style.display = "none";
+    }
+  });
+};
+
 window.addEventListener("load", (event) => {
   console.log("hello from javascript!");
+  toggleDropdown();
 });
