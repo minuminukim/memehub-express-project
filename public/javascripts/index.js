223952b6a7dcc27d0832d0eadad296166f75f070
anthony <awadams198@gmail.com>
2021-12-17 11:53:53 -0500

    like button working

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 import { toggleDropdown } from "./dropdown.js";
-// import { follow } from "./follow-button.js";
+import { follow } from "./follow-button.js";
 window.addEventListener("load", (event) => {
   console.log("hello from javascript!");
   toggleDropdown();
-  // follow();
+  follow();
 });
