e443ef5f22bd1623486fd6a557bbb0e6c71fe7f5
Minu Kim <minumoseskim@gmail.com>
2021-12-18 16:04:36 -0500

    Revise migration to return promise

--- db/migrations/20211218190912-modify-follows-add-id.js
+++ db/migrations/20211218190912-modify-follows-add-id.js
@@ -1,22 +1,18 @@
 "use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    queryInterface.addColumn("Follows", "id", {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    });
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn("Follows", "id", {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      }),
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return Promise.all([queryInterface.removeColumn("Follows", "id")]);
   },
 };
