5180d02b88c60ff7c9fc81c66e808fcb179ece62
will <w.duffy@outlook.com>
2021-12-19 10:54:57 -0600

    updated the search router to find by keyword

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
-
+const { User, Meme, Comment, Like, Follow } = require("../db/models");
 const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("../utils");
 
@@ -8,8 +8,26 @@ const router = express.Router();
 
 router.get('/', (req, res) =>{
     res.render('search-page', {
-        title: "Search"
+        title: "Search",
     })
 });
+
+router.post('/', asyncHandler(async (req, res) =>{
+
+    const { search } = req.body
+
+
+    const findMeme = await Meme.findAll({
+        where: {
+            headline: `${search}`
+        }
+    })
+    console.log(search === 'hello world')
+    console.log(findMeme)
+    res.render('search-page', {
+        title: "Search",
+        findMeme,
+    })
+}));
 
 module.exports = router;
