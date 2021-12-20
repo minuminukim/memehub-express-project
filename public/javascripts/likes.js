02bba81c140ff58ae079ffa4c7a1ce7da10df57d
will <w.duffy@outlook.com>
2021-12-19 22:25:46 -0600

    updated the like feature to work on the users/:id page

--- public/javascripts/likes.js
+++ public/javascripts/likes.js
@@ -1,48 +1,94 @@
-// window.addEventListener("load", (event) => {
-console.log("likes javascript!");
-// });
+// // window.addEventListener("load", (event) => {
+// console.log("likes javascript!");
+// // });
 
-// console.log(typeof count);
-// console.log(typeof counted);
-// likeCounter.innerHTML = count;
-// console.log("likecounterx", count);
+// // console.log(typeof count);
+// // console.log(typeof counted);
+// // likeCounter.innerHTML = count;
+// // console.log("likecounterx", count);
 
-const addLike = document.querySelector(".likeButton");
-// console.log("addLike", addLike);
+// const addLike = document.querySelector(".likeButton");
+// // console.log("addLike", addLike);
 
-//below lines are for creating the event on every button
-// window.addEventListener("DOMContentLoaded", () => {
-// const addLikeButtonListener = (button) => {
+// //below lines are for creating the event on every button
+// // window.addEventListener("DOMContentLoaded", () => {
+// // const addLikeButtonListener = (button) => {
 
-addLike.addEventListener("click", async (e) => {
-  console.log("test")
-  const memeId = e.target.id.split("-")[1]; // [like, 3]
-  let likeCounter = document.querySelector("#likeCount");
+// addLike.addEventListener("click", async (e) => {
+//   console.log("test")
+//   const memeId = e.target.id.split("-")[1]; // [like, 3]
+//   let likeCounter = document.querySelector("#likeCount");
 
-    let count = likeCounter.innerHTML;
-    count = parseInt(count.split(": ")[1], 10);
+//     let count = likeCounter.innerHTML;
+//     count = parseInt(count.split(": ")[1], 10);
 
-  // let count = likeCounter.innerHTML;
-  // console.log("likeCounter", count);
+//   // let count = likeCounter.innerHTML;
+//   // console.log("likeCounter", count);
 
-  const res = await fetch("/api/likes", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ memeId }),
-  });
-  const data = await res.json();
-  // console.log(data);
-  if (data.message === "liked") {
-    count += 1;
-    likeCounter.innerHTML = `Likes: ${count}`;
-    e.target.innerText = "Unlike";
-  } else {
-    count -= 1;
-    likeCounter.innerHTML = `Likes: ${count}`;
-    e.target.innerText = "Like";
+//   const res = await fetch("/api/likes", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ memeId }),
+//   });
+//   const data = await res.json();
+//   // console.log(data);
+//   if (data.message === "liked") {
+//     count += 1;
+//     likeCounter.innerHTML = `Likes: ${count}`;
+//     e.target.innerText = "Unlike";
+//   } else {
+//     count -= 1;
+//     likeCounter.innerHTML = `Likes: ${count}`;
+//     e.target.innerText = "Like";
+//   }
+// })
+// }
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  const addLikeButtonListener = (button) => {
+
+    button.addEventListener("click", async (e) => {
+      console.log("test")
+      const memeId = e.target.id.split("-")[1]; // [like, 3]
+      console.log("memeId", memeId)
+      let likeCounter = document.getElementById(`likeCount-${memeId}`);
+
+      let count = likeCounter.innerHTML;
+      console.log("count", count)
+      count = parseInt(count.split(": ")[1], 10);
+
+      const res = await fetch("/api/likes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ memeId }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.message === "liked") {
+        count += 1;
+        likeCounter.innerHTML = `Likes: ${count}`;
+        e.target.innerText = "Unlike";
+      } else {
+        count -= 1;
+        likeCounter.innerHTML = `Likes: ${count}`;
+        e.target.innerText = "Like";
+      }
+    })
+
+
+
+  }
+
+
+
+  const addLikeButtons = document.querySelectorAll(".likeButton");
+
+  for (let i = 0; i < addLikeButtons.length; i++) {
+    const button = addLikeButtons[i]
+    addLikeButtonListener(button);
   }
 })
-// }
 
 //below code is used with the commented code above on 14 and 15
 // const likeButton = document.querySelectorAll(".likeButton");
