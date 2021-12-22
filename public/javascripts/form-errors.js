d32f1de61bb7b49e1b182acf12ee135016d53410
Minu Kim <minumoseskim@gmail.com>
2021-12-22 04:13:32 -0500

    Trying to fix demo button for sign up form

--- public/javascripts/form-errors.js
+++ public/javascripts/form-errors.js
@@ -3,39 +3,39 @@ const newUserError = () => {
 
   errorList.forEach((error) => {
     const message = error.innerText;
-    console.log(message);
+
     let label;
     let input;
     switch (message) {
-      case "Please enter Username":
+      case "Please enter your Username.":
         label = document.querySelector(`label[for="username"]`);
         input = document.querySelector("#username");
         break;
-      case "Please enter First Name":
+      case "Please enter your First Name.":
         label = document.querySelector(`label[for="firstName"]`);
         input = document.querySelector("#firstName");
         break;
-      case "Please enter Last Name":
+      case "Please enter your Last Name.":
         label = document.querySelector(`label[for="lastName"]`);
         input = document.querySelector("#lastName");
         break;
-      case "Please enter Email Address":
+      case "Please enter your Email.":
         label = document.querySelector(`label[for="email"]`);
         input = document.querySelector("#email");
         break;
-      case "Email Address is not a valid email":
+      case "Email Address is not a valid email.":
         label = document.querySelector(`label[for="email"]`);
         input = document.querySelector("#email");
         break;
-      case "Please enter Password":
+      case "Please enter your Password.":
         label = document.querySelector(`label[for="password"]`);
         input = document.querySelector("#password");
         break;
-      case `Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")`:
+      case `Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*").`:
         label = document.querySelector(`label[for="password"]`);
         input = document.querySelector("#password");
         break;
-      case "Please confirm Password":
+      case "Please confirm Password.":
         label = document.querySelector(`label[for="confirmPassword"]`);
         input = document.querySelector("#confirmPassword");
         break;
@@ -44,6 +44,7 @@ const newUserError = () => {
       label.style.color = "#c94a4a";
       input.style.borderBottom = "1px solid #c94a4a";
 
+      // TODO check if div has a sibling for fields w/ more than one message
       const renderedMessage = document.createElement("div");
       renderedMessage.innerText = `${message}`;
       renderedMessage.classList.add("error-message");
--- validators/user-validators.js
+++ validators/user-validators.js
@@ -4,52 +4,52 @@ const db = require("../db/models");
 const userValidators = [
   check("username")
     .exists({ checkFalsy: true })
-    .withMessage("Please enter Username")
+    .withMessage("Please enter your Username.")
     .isLength({ max: 30 })
-    .withMessage("Username must not be more than 30 characters long"),
+    .withMessage("Username must not be more than 30 characters long."),
   check("firstName")
     .exists({ checkFalsy: true })
-    .withMessage("Please enter First Name")
+    .withMessage("Please enter your First Name.")
     .isLength({ max: 50 })
-    .withMessage("First Name must not be more than 50 characters long"),
+    .withMessage("First Name must not be more than 50 characters long."),
   check("lastName")
     .exists({ checkFalsy: true })
-    .withMessage("Please enter Last Name")
+    .withMessage("Please enter your Last Name.")
     .isLength({ max: 50 })
-    .withMessage("Last Name must not be more than 50 characters long"),
+    .withMessage("Last Name must not be more than 50 characters long."),
   check("email")
     .exists({ checkFalsy: true })
-    .withMessage("Please enter Email Address")
+    .withMessage("Please enter your Email.")
     .isLength({ max: 255 })
-    .withMessage("Email Address must not be more than 255 characters long")
+    .withMessage("Email Address must not be more than 255 characters long.")
     .isEmail()
-    .withMessage("Email Address is not a valid email")
+    .withMessage("Email Address is not a valid email.")
     .custom((value) => {
       return db.User.findOne({ where: { email: value } }).then((user) => {
         if (user) {
           return Promise.reject(
-            "The provided Email Address is already in use by another account"
+            "The provided Email Address is already in use by another account."
           );
         }
       });
     }),
   check("password")
     .exists({ checkFalsy: true })
-    .withMessage("Please enter Password")
+    .withMessage("Please enter your Password.")
     .isLength({ max: 50 })
-    .withMessage("Password must not be more than 50 characters long")
+    .withMessage("Password must not be more than 50 characters long.")
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
     .withMessage(
-      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*").'
     ),
   check("confirmPassword")
     .exists({ checkFalsy: true })
-    .withMessage("Please confirm Password")
+    .withMessage("Please confirm your Password.")
     .isLength({ max: 50 })
-    .withMessage("Confirm Password must not be more than 50 characters long")
+    .withMessage("Confirm Password must not be more than 50 characters long.")
     .custom((value, { req }) => {
       if (value !== req.body.password) {
-        throw new Error("Confirm Password does not match Password");
+        throw new Error("Confirm Password does not match Password.");
       }
       return true;
     }),
