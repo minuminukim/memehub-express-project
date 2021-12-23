f7da06ec11b17da8c594d72691b28e00cff54111
Minu Kim <minumoseskim@gmail.com>
2021-12-22 20:41:57 -0500

    Working on likes styles: added icons and event listener for click and hover events

--- public/javascripts/likes.js
+++ public/javascripts/likes.js
@@ -45,51 +45,62 @@
 // }
 
 window.addEventListener("DOMContentLoaded", () => {
-
   const addLikeButtonListener = (button) => {
-
     button.addEventListener("click", async (e) => {
-      console.log("test")
+      console.log("test");
       const memeId = e.target.id.split("-")[1]; // [like, 3]
-      console.log("memeId", memeId)
+      console.log("memeId", memeId);
       let likeCounter = document.getElementById(`likeCount-${memeId}`);
 
-      let count = likeCounter.innerHTML;
-      console.log("count", count)
-      count = parseInt(count.split(": ")[1], 10);
+      let count = parseInt(likeCounter.innerText, 10);
+      // let count = likeCounter.innerHTML;
+      console.log("count", count);
+      // count = parseInt(count.split(": ")[1], 10);
 
       const res = await fetch("/api/likes", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ memeId }),
       });
+
       const data = await res.json();
       console.log(data);
       if (data.message === "liked") {
         count += 1;
-        likeCounter.innerHTML = `Likes: ${count}`;
-        e.target.innerText = "Unlike";
+        likeCounter.innerText = `${count}`;
+        button.classList.add("invert");
+        // e.target.innerText = "Unlike";
       } else {
         count -= 1;
-        likeCounter.innerHTML = `Likes: ${count}`;
-        e.target.innerText = "Like";
+        likeCounter.innerHTML = `${count}`;
+        // e.target.innerText = "Like";
       }
-    })
-
-
-
-  }
-
-
+    });
+  };
 
   const addLikeButtons = document.querySelectorAll(".likeButton");
 
   for (let i = 0; i < addLikeButtons.length; i++) {
-    const button = addLikeButtons[i]
+    const button = addLikeButtons[i];
     addLikeButtonListener(button);
+    toggleHiddenBubble(button);
   }
-})
+});
 
+
+const toggleHiddenBubble = (button) => {
+  const memeId = button.id.split("-")[1];
+  const bubble = document.getElementById(`bubble-${memeId}`);
+
+  button.addEventListener("mouseover", (e) => {
+    console.log(bubble);
+    bubble.classList.remove("hidden");
+  })
+
+  button.addEventListener("mouseout", (e) => {
+    bubble.classList.add("hidden");
+  })
+};
 //below code is used with the commented code above on 14 and 15
 // const likeButton = document.querySelectorAll(".likeButton");
 // for (let i = 0; i < likeButton.length; i++) {
@@ -97,7 +108,6 @@ window.addEventListener("DOMContentLoaded", () => {
 //   addLikeButtonListener(button);
 // }
 // })
-
 
 /*grab the ele off the page
 cant ele++ or --
