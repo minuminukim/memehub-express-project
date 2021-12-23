4a993c102522033471255e387157ad582175daf6
will <w.duffy@outlook.com>
2021-12-22 23:16:49 -0600

    updated scroll navbar transition height

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,16 +1,14 @@
 export const docScroll = () =>{
 
     window.addEventListener('scroll', (event) => {
-
         let nav = document.querySelector("nav.landing-nav")
-
         let color = 'white';
         let originalColor = '#ffc017'
 
-        if (scrollY >= 500){
+        if (scrollY >= 600){
             nav.style.background = color;
         }
-        if (scrollY < 500) {
+        if (scrollY < 600) {
             nav.style.background = originalColor;
         }
     });
