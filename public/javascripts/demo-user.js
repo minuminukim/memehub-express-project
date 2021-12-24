f8b39eb9920f926d9fef8db19b8d11a8a79a5bf1
Minu Kim <minumoseskim@gmail.com>
2021-12-23 23:41:26 -0500

    Media query on user forms and demo sign in script

--- public/javascripts/demo-user.js
+++ public/javascripts/demo-user.js
@@ -2,7 +2,13 @@ const demoSignIn = () => {
   const button = document.querySelector(".demo-button");
 
   button.addEventListener("click", (event) => {
-    // event.preventDefault();
+    const path = window.location.href.toString().trim().split("/").pop();
+
+    if (path !== "sign-in") {
+      event.preventDefault();
+      window.location.replace("/users/sign-in");
+    }
+
     const emailField = document.querySelector("#email");
     const passwordField = document.querySelector("#password");
     emailField.value = "hello@world.com";
