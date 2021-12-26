5fd3ccfd4977da6c45baafd1aee47201300ba4c9
Minu Kim <minumoseskim@gmail.com>
2021-12-26 17:58:57 -0500

    Re-factor sign in route to redirect to /you

--- public/javascripts/demo-user.js
+++ public/javascripts/demo-user.js
@@ -2,17 +2,20 @@ const demoSignIn = () => {
   const button = document.querySelector(".demo-button");
 
   button.addEventListener("click", (event) => {
+    event.preventDefault();
     const path = window.location.href.toString().trim().split("/").pop();
 
     if (path !== "sign-in") {
-      event.preventDefault();
       window.location.replace("/users/sign-in");
-    }
+      return;
+    } else {
+      const emailField = document.querySelector("#email");
+      const passwordField = document.querySelector("#password");
+      emailField.value = "hello@world.com";
+      passwordField.value = "password";
 
-    const emailField = document.querySelector("#email");
-    const passwordField = document.querySelector("#password");
-    emailField.value = "hello@world.com";
-    passwordField.value = "password";
+      document.signIn.submit();
+    }
   });
 };
 
